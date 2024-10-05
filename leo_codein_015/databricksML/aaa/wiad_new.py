39283d89eeb5fb2bc28d8eda30398a8b49b5b9af
leohuangcompany <leohuangcompany>
2024-10-05 09:57:49 +0000

    import

---
+++ leo_codein_015/databricksML/aaa/wiad_new.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %pip install dbdemos
+
+# COMMAND ----------
+
+import dbdemos
+dbdemos.install('uc-04-system-tables', catalog='main', schema='billing_forecast')
+
+# COMMAND ----------
+
+
